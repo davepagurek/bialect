f66d789dbbe632a556389883701f4929672fb132
Dave Pagurek <davepagurek@gmail.com>
2024-06-29 10:08:52 -0400

    Adjust title + description

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <GameContextProvider>
         <Stack maxWidth="600px" spacing={3} margin="20px auto" padding={1}>
           <h1>Bialect</h1>
+          <p>Create two words out of your hand of letters.<br />You don't need to use every letter, but more letters gives you more points!</p>
           <GameView />
         </Stack>
       </GameContextProvider>
