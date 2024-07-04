8e5177334481cb3ddd4aa379eb98ca9c86a2fe46
Dave Pagurek <davepagurek@gmail.com>
2024-07-04 09:08:00 -0400

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+function isCacheable(request) {
+  const url = new URL(request.url);
+  return !url.pathname.endsWith(".json");
+}
+
+async function cacheFirstWithRefresh(request) {
+  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+    if (networkResponse.ok) {
+      const cache = await caches.open("Bialect_1");
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  });
+
+  return (await caches.match(request)) || (await fetchResponsePromise);
+}
+
+self.addEventListener("fetch", (event) => {
+  if (isCacheable(event.request)) {
+    event.respondWith(cacheFirstWithRefresh(event.request));
+  }
+});
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <App />
